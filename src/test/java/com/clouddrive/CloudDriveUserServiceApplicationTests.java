a89826109cec9962bad4faf68bd99a8fabb09d4c
Akash <akash.vyavhare@veracity-india.com>
2025-11-06 20:36:40 +0530

    initail commiit

---
+++ src/test/java/com/clouddrive/CloudDriveUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDriveUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
